5a4bb86fd29f8e9026cb922a38d8bb8a0cb55a0f
Andre Luiz <aribeiro.rj@gmail.com>
2025-12-17 09:15:25 -0300

    chore(tailwind): add initial tailwind configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center h-screen text-4xl font-bold">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
